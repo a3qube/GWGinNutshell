64deb8e930cb3181b218750c8c80178f5361965f
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2018-04-08 14:14:14 -0700

    Highlighting the lifecycle

--- js/main.js
+++ js/main.js
@@ -1,39 +1,55 @@
 
 
-  // if ('serviceWorker' in navigator) {//Check if browser supports service worker
-  //   navigator.serviceWorker.register('js/sw/index.js')
-  //   .then(function(reg) {
-  //     console.log('Registration worked!'+reg) ;
+  if ('serviceWorker' in navigator) {//Check if browser supports service worker
+    navigator.serviceWorker.register('js/sw/index.js')
+    .then(function(registration) {
+      console.log('registrationistration worked!',registration) ;
+      // if(!navigator.serviceWorker.controller){
+      //   return;
+      // }
+      navigator.serviceWorker.addEventListener("controllerchange",function(){
+        location.reload();
+      });
   
-  //     if(!navigator.serviceWorker.controller){
-  //       return;
-  //     }
-  //     navigator.serviceWorker.addEventListener("controllerchange",function(){
-  //       location.reload();
-  //     });
-  
-  //     let oInstalling = reg.installing;
-  //     console.log(sw.state);
+      let oInstalling = registration.installing;
+      let sw;
+      if (registration.installing) {
+        sw = registration.installing;
+        document.querySelector('#installing').classList.add('highlight');                    
+    } else if (registration.waiting) {
+        sw = registration.waiting;  
+        document.querySelector('#waiting').classList.add('highlight');                    
+    } else if (registration.active) {
+        sw = registration.active;
+        document.querySelector('#active').classList.add('highlight');
+    }
+    if(sw){
+      sw.addEventListener('statechange',function(e){
+        console.log(e.target.state)
+      })
+    }
 
-  //      /*
-  //     TODO - when waiting
-  //     */
+
+
+       /*
+      TODO - when waiting
+      */
         
 
-  //       /*
-  //     TODO - when state Change
-  //     */
+        /*
+      TODO - when state Change
+      */
   
         
-  //     /*
-  //     TODO - UpdateFound
-  //     */
+      /*
+      TODO - UpdateFound
+      */
       
     
-  //   }).catch(function() {
-  //     console.log('Registration failed!');
-  //   });
-  //     }
+    }).catch(function() {
+      console.log('Registration failed!');
+    });
+      }
 
 
 
